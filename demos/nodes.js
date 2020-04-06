a147779dee24771dd8072625210d95d0a3411008
Utry Máté <36498701+utrymate@users.noreply.github.com>
2020-04-06 18:38:36 +0200

    Add functions to the different classes

--- demos/nodes.js
+++ demos/nodes.js
@@ -4,6 +4,9 @@ class Node {
         this.y = y;
         this.id = id;
     }
+    printNodeID(context, x, y) {
+        context.fillText("Node's ID: "+this.id, x, y);
+    }
 }
 
 class Rectangle extends Node{
@@ -16,19 +19,37 @@ class Rectangle extends Node{
 
     draw(context) {
         context.beginPath();
-        context.fillStyle = "yellow";
+        //context.fillStyle = "yellow";
+        context.fillStyle = 'rgb(255, 223, 0)';
         context.fillRect(this.x, this.y, this.width, this.height);
         context.stroke();
         context.fillStyle = "blue";
-        context.globalCompositeOperation='source-over';
+        //context.globalCompositeOperation='source-over';
         context.fillText(this.text,this.x+this.width/2, this.y+this.height/2);
     }
+    // parameters: mouse[x] & mouse[y]
+    // mouse["x"] -> x ; node["x"] -> this.x
+    isMouseOnNode(x, y) {
+        return x >= this.x
+            && x <= this.x +this.width //+width hogy az egész node-ot húzni tudd
+            && y >= this.y
+            && y <= this.y + this.height;
+    }
+
+    widen(length){
+        if(15 > this.width / length)
+        {
+            this.width += 14;
+        }
+    }
+
 }
 
 class Circle extends Node{
-    constructor(x, y, id, radius) {
+    constructor(x, y, id, radius, text) {
         super(x, y, id);
         this.radius = radius;
+        this.text = text;
     }
 
     draw(context) {
@@ -37,14 +58,25 @@ class Circle extends Node{
         context.fillStyle = "red";
         context.fill();
         context.fillStyle = "blue";
-        context.globalCompositeOperation='source-over';
+        //context.globalCompositeOperation='source-over';
         context.fillText(this.text, this.x, this.y);
+    }
+
+    isMouseOnNode(x, y) {
+        let shortestDistance = Math.abs(Math.sqrt(Math.pow(x-this.x, 2) + Math.pow(y-this.y, 2)));
+        return shortestDistance <= this.radius
+    }
+    widen(length){
+        if(8 > this.radius / length)
+        {
+            this.radius += 7;
+        }
     }
 }
 
 class Start extends Circle{
-    constructor(x, y, id, radius) {
-        super(x, y, id, radius);
+    constructor(x, y, id, radius, text) {
+        super(x, y, id, radius, text);
     }
 
     draw(context) {
@@ -54,14 +86,17 @@ class Start extends Circle{
         context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         context.stroke();
         context.fillStyle = "black";
-        context.globalCompositeOperation='source-over';
+        //context.globalCompositeOperation='source-over';
         context.fillText(this.text, this.x, this.y);
+    }
+    printNodeID(context, x, y) {
+        context.fillText("Start node's ID: "+this.id, x, y);
     }
 }
 
 class End extends Circle{
-    constructor(x, y, id, radius) {
-        super(x, y, id, radius);
+    constructor(x, y, id, radius, text) {
+        super(x, y, id, radius, text);
     }
 
     draw(context) {
@@ -71,15 +106,20 @@ class End extends Circle{
         context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         context.stroke();
         context.fillStyle = "black";
-        context.globalCompositeOperation='source-over';
+        //context.globalCompositeOperation='source-over';
         context.fillText(this.text, this.x, this.y);
     }
+
+    printNodeID(context, x, y) {
+        context.fillText("End node's ID: "+this.id, x, y);
+    }
+
 }
 
 function drawNodes(context) {
- for (var i = 0; i < nodes.length; ++i) {
-    var node = nodes[i];
-    node.draw(context);
+    for (let i = 0; i < nodes.length; ++i) {
+        let node = nodes[i];
+        node.draw(context);
     }
 }
 
